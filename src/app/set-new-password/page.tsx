743904884cbbb78f2c79c8534ca02f6554db459d
Anthony Kishan <anthony.kishan01@gmail.com>
2024-12-11 21:54:34 +0600

    Created New Pages

--- src/app/set-new-password/page.tsx
+++ src/app/set-new-password/page.tsx
@@ -5,7 +5,7 @@ import { Breadcrumb } from '@/components/ui/breadcrumb'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 
-export default function ForgotPasswordPage() {
+export default function SetNewPasswordPage() {
     return (
         <main>
             <Breadcrumb
@@ -17,18 +17,24 @@ export default function ForgotPasswordPage() {
                 <div className="w-full max-w-md space-y-8">
                     <form className="space-y-6 p-6 rounded-lg shadow-lg">
                         <div className="text-center">
-                            <h1 className="text-3xl font-bold">Forgot Password</h1>
+                            <h1 className="text-3xl font-bold">Set New Password</h1>
                         </div>
                         <div>
                             <Input
-                                type="email"
-                                placeholder="Email"
+                                type="password"
+                                placeholder="Enter new password"
+                            />
+                        </div>
+                        <div>
+                            <Input
+                                type="password"
+                                placeholder="Confirm new password"
                             />
                         </div>
 
                         <Link href="/set-new-password" className='w-full rounded-full '>
-                            <Button type='submit'  className='w-full rounded-full mt-4'>
-                                Submit
+                            <Button type='submit' className='w-full rounded-full mt-4'>
+                                Create Account
                             </Button>
                         </Link>
                     </form>
